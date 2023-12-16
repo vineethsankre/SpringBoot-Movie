6c217ffdc1e8cace2258ecd40bc90a822f5ee657
vineethsankre <lsvineethsankre@gmail.com>
2023-12-16 06:15:33 +0000

    Attributes and methods declared

---
+++ src/main/java/com/example/movie/MovieService.java
@@ -0,0 +1,34 @@
+/*
+ * You can use the following import statements
+ * 
+ * import org.springframework.web.server.ResponseStatusException;
+ * import org.springframework.http.HttpStatus;
+ * 
+ */
+
+package com.example.movie;
+
+import com.example.movie.Movie;
+import com.example.movie.MovieRepository;
+
+import java.util.*;
+
+// Do not modify the below code
+
+public class MovieService implements MovieRepository {
+
+    private static HashMap<Integer, Movie> movieList = new HashMap<>();
+
+    public MovieService() {
+        movieList.put(1, new Movie(1, "Avengers: Endgame", "Robert Downey Jr."));
+        movieList.put(2, new Movie(2, "Avatar", "Sam Worthington"));
+        movieList.put(3, new Movie(3, "Titanic", "Leonardo DiCaprio"));
+        movieList.put(4, new Movie(4, "Star Wars: The Force Awakens", "Daisy Ridley"));
+        movieList.put(5, new Movie(5, "Jurassic World", "Chris Pratt"));
+    }
+
+    // Do not modify the above code
+
+    // Write your code here
+
+}
